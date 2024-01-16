31a08a5e2ea261dfa00c8de68fd2edd25b2463e5
axesaibo <128742572+axesaibo@users.noreply.github.com>
2024-01-16 20:37:33 +0500

    Add files via upload

---
+++ Policy.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1>privacy policy</h1>
+
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
